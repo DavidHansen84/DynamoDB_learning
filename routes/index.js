b860a69efc1972f34c42aaa1cb963ba3c23e2f56
David Hansen <dracozlair@gmail.com>
2023-11-01 15:12:34 +0100

    content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const CyclicDB = require('@cyclic.sh/dynamodb')
+const db = CyclicDB(process.env.CYCLIC_DB)
+let contentCollection = db.collection('content')
+
+router.get("/", async (req, res, next) => {
+  let content = await contentCollection.get("content")
+
+  if (content == null) {
+    res.json({
+      status: "fail",
+      message: "Content is empty"
+    });
+  } else {
+    let contentValue = content.props.value
+    res.json({
+      status: "success",
+      result: contentValue,
+    });
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const { content } = req.body;
+  if (content == null) {
+    res.status(400).send("Content not provided");
+    return;
+  }
+  await contentCollection.set("content", {
+    value: content
+  })
+  res.json({
+    status: "success",
+    content: content,
+  });
 });
 
 module.exports = router;
